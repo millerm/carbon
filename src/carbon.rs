89186190d18d7e0848ad7cab68d5dab985d09f80
millerm <marshalldavidmiller@gmail.com>
2022-01-07 20:50:13 -0800

    Add new structs

--- src/carbon.rs
+++ src/carbon.rs
@@ -2,6 +2,23 @@ use std::fs::{File, OpenOptions};
 use std::io;
 use std::path::Path;
 
+/// Represents a Blueprint which right now is a name and a path to a file
+#[derive(Debug)]
+pub struct Blueprint {
+  name: String,
+  path: String,
+}
+
+/// A configuration file will have a list of Blueprints
+type BlueprintList = Vec<Box<Blueprint>>;
+
+/// Represents the contents of a Configuration file
+#[derive(Debug)]
+pub struct Configuration {
+  blueprints: BlueprintList,
+}
+
+/// TODO(@millerm): Too meta
 #[derive(Debug)]
 pub struct Carbon {
   config_file: File,
@@ -15,14 +32,38 @@ impl Carbon {
       .create(true)
       .append(true)
       .open(path)?;
+
+    // Here we test adding blueprints.
+    // These will be parsed from a configuration file.
+    let test_blueprint = Blueprint {
+      name: String::from("test1"),
+      path: String::from("test1.json"),
+    };
+
+    let test_blueprint2 = Blueprint {
+      name: String::from("test2"),
+      path: String::from("test2.json"),
+    };
+
+    let mut bp = BlueprintList::new();
+
+    bp.push(Box::new(test_blueprint));
+    bp.push(Box::new(test_blueprint2));
+
+    let conf = Configuration { blueprints: bp };
+
+    println!("Config --> {:?}", conf);
+
     Ok(Carbon { config_file })
   }
+
   pub fn generate(&mut self, blueprint: &str, destination_path: &Path) -> io::Result<()> {
     println!(
       "Generating blueprint {} at path {}...",
       blueprint,
       destination_path.display()
     );
+
     Ok(())
   }
 }
